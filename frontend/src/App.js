ca8719f7c12a3a630b8a571f7b5dca8c26361192
AhmadAzmii <ah.azmi.hasann@gmail.com>
2024-07-18 18:37:18 +0300

    starter Code

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+
+
+//===============================================================
+
+const App = () => {
+  return (
+    <div className="App">
+      
+      <Routes>
+        {/* <Route path={"/"} element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/newArticle" element={<NewArticle />} /> */}
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
