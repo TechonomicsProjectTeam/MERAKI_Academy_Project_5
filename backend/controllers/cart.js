5a528598ab196b08d47614bf8e1f9490e003f713
NataliTawalbehh <natali.m.tawalbeh@gmail.com>
2024-07-20 20:45:40 +0300

    project5

--- backend/controllers/cart.js
+++
@@ -1,8 +0,0 @@
-const pool = require("../models/db");
-
-const addCart = (req , res) =>{
-    const {price , quantity} = req.body;
-    const user_id = req.token.userId;
-    const {product_id} = req.params;
-    
-}
