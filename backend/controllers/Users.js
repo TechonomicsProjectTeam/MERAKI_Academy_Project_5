439f24a58cf6e9c2888fa3e36fda7081f67ca1ba
NataliTawalbehh <natali.m.tawalbeh@gmail.com>
2024-07-21 00:35:19 +0300

    register & login

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -3,13 +3,165 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const saltRounds = parseInt(process.env.SALT);
 
-const register = (req, res) => {};
+const register = async (req, res) => {
+  const { first_name, last_name, username, phone_number, email, password, images } =
+    req.body;
+  const role_id = '1'// edit the value of role_id depend on role id in role table .
+  const encryptedPassword = await bcrypt.hash(password, saltRounds);
+  const query = `INSERT INTO users (first_name, last_name, username, phone_number, email, password, role_id , images) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) returning *`;
+  const data = [
+    first_name,
+    last_name,
+    username,
+    phone_number,
+    email.toLowerCase(),
+    encryptedPassword,
+    role_id,
+    images,
+  ];
+  pool
+    .query(query, data)
+    .then((result) => {
+      console.log("fisrt result is ok", result);
+      if(result.rowCount){
+      const users_id = result.rows[0].user_id;
+      const newCart = `INSERT INTO cart (user_id) VALUES ($1) RETURNING *`
+      pool.query(newCart, [users_id])
+        .then((result) => {
+          console.log("second result is ok");
+          res.status(200).json({
+            success: true,
+            message: "Account Cart created successfully",
+            cart: result.rows
+          });
+        })
+        .catch((error) => {
+          res.status(409).json({
+            success: false,
+            message: "error in creating cart",
+            error: error.message
+          })
+        })
 
-const login = (req, res) => {};
+      res.status(201).json({
+        success: true,
+        message: "Account Created Successfully",
+        user: result.rows[0]
+      })
+    }
+    })
+    .catch((err) => {
+      res.status(409).json({
+        success: false,
+        message: "The email already exists",
+        err: err.message,
+      });
+    });
+};
 
-const updateUserById = (req, res) => {};
 
-const deleteUserById = (req, res) => {};
+const login = (req, res) => {
+  const password = req.body.password;
+  const email = req.body.email;
+  const query = ` SELECT users.*, roles.role_name
+    FROM users 
+    JOIN roles ON users.role_id = roles.role_id 
+    WHERE users.email = $1 `;
+  const data = [email.toLowerCase()];
+  pool
+    .query(query, data)
+    .then((result) => {
+      if (result.rows.length) {
+        bcrypt.compare(password, result.rows[0].password, (err, response) => {
+          if (err) res.json(err);
+          if (response) {
+            const payload = {
+              userId: result.rows[0].user_id,
+              username: result.rows[0].username,
+              role: result.rows[0].role_id,
+              role_name: result.rows[0].role_name,
+            };
+            const options = { expiresIn: "1d" };
+            const secret = process.env.SECRET;
+            const token = jwt.sign(payload, secret, options);
+            if (token) {
+              return res.status(200).json({
+                token,
+                success: true,
+                message: `Valid login credentials`,
+                userId: result.rows[0].user_id
+              });
+            } else {
+              throw Error;
+            }
+          } else {
+            res.status(403).json({
+              success: false,
+              message: `The email doesn’t exist or the password you’ve entered is incorrect`,
+            });
+          }
+        });
+      } else throw Error;
+    })
+    .catch((err) => {
+      res.status(403).json({
+        success: false,
+        message:
+          "The email doesn’t exist or the password you’ve entered is incorrect",
+        err,
+      });
+    });
+};
+
+const updateUserById = (req, res) => {
+  const user_id = req.params.user_id;
+  const { first_name, last_name, username, phone_number, email, password, images } =
+    req.body;
+
+  pool.query(`UPDATE users SET  first_name = $1, last_name= $2, username = $3, phone_number =$4, email =$5, password =$6 , images =$7 WHERE user_id =$8 RETURNING * `,
+    [first_name, last_name, username, phone_number, email, password, images, user_id])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return res.status(403).json("user not found")
+      }
+      res.status(200).json({
+        success: true,
+        message: "Account updated successfully",
+        updateUser: result.rows[0]
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message:
+          "server error",
+        err,
+      });
+    });
+};
+
+const deleteUserById = (req, res) => {
+  const user_id = req.params.user_id;
+  pool.query(`DELETE FROM users WHERE user_id = $1 RETURNING *`, [user_id])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return res.status(403).json("user not found")
+      }
+      res.status(200).json({
+        success: true,
+        message: "Account deleted successfully",
+        updateUser: result.rows[0]
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message:
+          "server error",
+        err,
+      });
+    });
+};
 
 module.exports = {
   register,
