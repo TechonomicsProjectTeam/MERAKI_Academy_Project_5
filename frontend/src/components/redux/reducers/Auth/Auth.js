9f176a592e7fe1e05596b9bf3311ee0e777544b9
NataliTawalbehh <natali.m.tawalbeh@gmail.com>
2024-07-24 14:45:25 +0300

    athSlice

---
+++ frontend/src/components/redux/reducers/Auth/Auth.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+    name:"auth",
+    initialState:{
+        token : localStorage.getItem("token") || null,
+        userId : localStorage.getItem("userId") || null,
+        isLoggedIn : localStorage.getItem("token")?false:true
+    },
+
+    reducers:{
+        setLogin:(state , action)=>{
+            state.token = action.payload;
+            state.isLoggedIn = true;
+            localStorage.setItem('token',action.payload)
+        },
+        setUserId:(state , action)=>{
+            state.userId = action.payload;
+            localStorage.setItem('userId',action.payload)
+        },
+        setLogout:(state , action)=>{
+            state.token = null;
+            state.userId = null;
+            state.isLoggedIn = false
+            localStorage.clear()
+        }
+    }
+})
+export const {setLogin , setUserId , setLogout} = authSlice.actions;
+export default authSlice.reducer;
--- frontend/src/components/redux/store.js
+++ frontend/src/components/redux/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./reducers/Auth/Auth";
 export default configureStore({
     reducer: {
+        ath: authReducer,
     }
 })
