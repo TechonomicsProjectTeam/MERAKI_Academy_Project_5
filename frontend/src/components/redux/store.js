f97917ef842e9deef3bf0814b8b8d1a773e8f4a7
NataliTawalbehh <natali.m.tawalbeh@gmail.com>
2024-07-24 14:49:39 +0300

    cartSlice

--- frontend/src/components/redux/store.js
+++ frontend/src/components/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/Auth/Auth";
+import cartsReducer from "./reducers/Carts/Carts"
 export default configureStore({
     reducer: {
         ath: authReducer,
+        cart: cartsReducer,
     }
 })
