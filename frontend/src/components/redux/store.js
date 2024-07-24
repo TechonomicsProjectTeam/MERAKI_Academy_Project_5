6229ad9cad64d757e3f90b4f6f6f7ea932e11320
NataliTawalbehh <natali.m.tawalbeh@gmail.com>
2024-07-24 16:51:45 +0300

    import redux

--- frontend/src/components/redux/store.js
+++ frontend/src/components/redux/store.js
@@ -3,11 +3,13 @@ import authReducer from "./reducers/Auth/Auth";
 import shopsReducer from "./reducers/Shops/Shops";
 import usersReducer from "./reducers/Users/Users";
 import cartsReducer from "./reducers/Carts/Carts";
+import ProductsReducer from "./reducers/Products/Products";
 export default configureStore({
     reducer: {
         auth: authReducer,
         shops: shopsReducer,
         user: usersReducer,
         cart: cartsReducer,
+        product: ProductsReducer,
     }
 });
