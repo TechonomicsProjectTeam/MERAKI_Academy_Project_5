118f1306dc288edcc6e2f8253499116ce4617bd9
NataliTawalbehh <natali.m.tawalbeh@gmail.com>
2024-07-25 19:44:39 +0300

    navbar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,18 +1,50 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import {useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { setLogout } from '../../redux/reducers/Auth/Auth';
 import "../NavBar/Style.css"
 const NavBar = () => {
+  const dispatch = useDispatch();
+
   const username = useSelector((state) => state.auth.username);
   const imageUrl = useSelector((state) => state.auth.images);
+  const isLoggedIn = useSelector((state)=> state.auth.isLoggedIn);
+
+  const handleLogout = () =>{
+    dispatch(setLogout())
+  }
 console.log(username);
   return (
     <div className="NavBar">
-      <div className="user-info">
-        {imageUrl && <img src={imageUrl} alt="User" className="user-image" />}
-        {username && <span className="user-name">{username}</span>}
-      </div>
-      NavBar Component
+    <div className="user-info">
+      {imageUrl && <img src={imageUrl} alt="User" className="user-image" />}
+      {username && <span className="user-name">Welcome  {username} !!</span>}
     </div>
+    <nav>
+      <NavLink to="/user-dashboard" className={({ isActive }) => (isActive ? 'active' : '')}>
+        User Dashboard
+      </NavLink>
+      {isLoggedIn && (
+        <>
+          <NavLink to="/carts" className={({ isActive }) => (isActive ? 'active' : '')}>
+            Carts
+          </NavLink>
+          <NavLink to="/orders" className={({ isActive }) => (isActive ? 'active' : '')}>
+            Orders
+          </NavLink>
+          <NavLink to="/reviews" className={({ isActive }) => (isActive ? 'active' : '')}>
+            Reviews
+          </NavLink>
+          <NavLink to="/user-settings" className={({ isActive }) => (isActive ? 'active' : '')}>
+            User Settings
+          </NavLink>
+          <NavLink to="/login" onClick={handleLogout}>
+            Logout
+          </NavLink>
+        </>
+      )}
+    </nav>
+  </div>
   );
 };
 
