e4682c2486fb2a4e704d02880c0fc73aa8208eea
AhmadAzmii <ah.azmi.hasann@gmail.com>
2024-07-25 20:01:45 +0300

    adding a logic to render the driver dashboard and the user dasboard depending on the roleId

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,50 +1,65 @@
 import React from 'react';
-import {useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { setLogout } from '../../redux/reducers/Auth/Auth';
-import "../NavBar/Style.css"
+import "../NavBar/Style.css";
+
 const NavBar = () => {
   const dispatch = useDispatch();
 
   const username = useSelector((state) => state.auth.username);
-  const imageUrl = useSelector((state) => state.auth.images);
-  const isLoggedIn = useSelector((state)=> state.auth.isLoggedIn);
+  const imageUrl = useSelector((state) => state.auth.imageUrl);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const roleId = useSelector((state) => state.auth.roleId);
 
-  const handleLogout = () =>{
-    dispatch(setLogout())
-  }
-console.log(username);
+  const handleLogout = () => {
+    dispatch(setLogout());
+  };
+
   return (
     <div className="NavBar">
-    <div className="user-info">
-      {imageUrl && <img src={imageUrl} alt="User" className="user-image" />}
-      {username && <span className="user-name">Welcome  {username} !!</span>}
-    </div>
-    <nav>
-      <NavLink to="/user-dashboard" className={({ isActive }) => (isActive ? 'active' : '')}>
-        User Dashboard
-      </NavLink>
-      {isLoggedIn && (
-        <>
-          <NavLink to="/carts" className={({ isActive }) => (isActive ? 'active' : '')}>
-            Carts
-          </NavLink>
-          <NavLink to="/orders" className={({ isActive }) => (isActive ? 'active' : '')}>
-            Orders
-          </NavLink>
-          <NavLink to="/reviews" className={({ isActive }) => (isActive ? 'active' : '')}>
-            Reviews
-          </NavLink>
-          <NavLink to="/user-settings" className={({ isActive }) => (isActive ? 'active' : '')}>
-            User Settings
-          </NavLink>
+      <div className="user-info">
+        {imageUrl && <img src={imageUrl} alt="User" className="user-image" />}
+        {username && <span className="user-name">Welcome {username} !!</span>}
+      </div>
+      <nav>
+        {isLoggedIn && roleId === 2 ? (
+          <>
+            <NavLink to="/driver-dashboard" className={({ isActive }) => (isActive ? 'active' : '')}>
+              Driver Dashboard
+            </NavLink>
+            <NavLink to="/login" onClick={handleLogout}>
+              Logout
+            </NavLink>
+          </>
+        ) : isLoggedIn ? (
+          <>
+            <NavLink to="/user-dashboard" className={({ isActive }) => (isActive ? 'active' : '')}>
+              User Dashboard
+            </NavLink>
+            <NavLink to="/carts" className={({ isActive }) => (isActive ? 'active' : '')}>
+              Carts
+            </NavLink>
+            <NavLink to="/orders" className={({ isActive }) => (isActive ? 'active' : '')}>
+              Orders
+            </NavLink>
+            <NavLink to="/reviews" className={({ isActive }) => (isActive ? 'active' : '')}>
+              Reviews
+            </NavLink>
+            <NavLink to="/user-settings" className={({ isActive }) => (isActive ? 'active' : '')}>
+              User Settings
+            </NavLink>
+            <NavLink to="/login" onClick={handleLogout}>
+              Logout
+            </NavLink>
+          </>
+        ) : (
           <NavLink to="/login" onClick={handleLogout}>
             Logout
           </NavLink>
-        </>
-      )}
-    </nav>
-  </div>
+        )}
+      </nav>
+    </div>
   );
 };
 
