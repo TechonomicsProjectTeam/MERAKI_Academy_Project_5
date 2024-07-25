cf5050eb963f682e3d4218e0c9861aedea2a2ca9
AhmadAzmii <ah.azmi.hasann@gmail.com>
2024-07-25 11:01:22 +0300

    fixing the router.js so i can render the page depending on the roleId

--- frontend/src/Routers/Router.js
+++ frontend/src/Routers/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 import MainLayout from '../Layouts/MainLayout';
 import Carts from '../Pages/Carts/Carts';
@@ -15,18 +15,58 @@ import Products from '../Pages/Products/Products';
 import Register from '../Pages/Register/Register';
 import ShopOwnerDashboard from '../Pages/ShopOwnerDashboard/ShopOwnerDashboard';
 import UserDashboard from '../Pages/UserDashboard/UserDashboard';
+import { useSelector } from 'react-redux';
+
+
+const ProtectedRoute = ({ element, role }) => {
+  const { isLoggedIn, roleId } = useSelector((state) => state.auth);
+  console.log(roleId);
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  if (role && role !== roleId) {
+    return <Navigate to="/" />;
+  }
+
+  return element;
+};
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children :[
+{      path:'user-dashboard', element: <ProtectedRoute element={<UserDashboard />} role={1} />,
+}    ]
+  },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children:[
+      { path: '/driver-dashboard',
+        element: <ProtectedRoute element={<DriverDashboard />} role={2} />,}
+    ]
+  },
+  {
+    path: '/',
     element: <MainLayout />,
     children: [
-      { path: 'carts', element: <Carts /> },
-      { path: 'orders', element: <Orders /> },
-      { path: 'reviews', element: <Reviews /> },
-      { path: 'user-settings', element: <UserSettings /> },
-      { path: 'admin-dashboard', element: <AdminDashboard /> },
-      { path: 'driver-dashboard', element: <DriverDashboard /> },
+      { path: 'carts', element: <ProtectedRoute element={<Carts />} /> },
+      { path: 'orders', element: <ProtectedRoute element={<Orders />} /> },
+      { path: 'reviews', element: <ProtectedRoute element={<Reviews />} /> },
+      { path: 'user-settings', element: <ProtectedRoute element={<UserSettings />} /> },
+      { path: 'admin-dashboard', element: <ProtectedRoute element={<AdminDashboard />} /> },
+      { path: 'driver-dashboard', element: <ProtectedRoute element={<DriverDashboard />} /> },
       { path: 'login', element: <Login /> },
       { path: 'owner-login', element: <OwnerLogin /> },
       { path: 'owner-register', element: <OwnerRegister /> },
