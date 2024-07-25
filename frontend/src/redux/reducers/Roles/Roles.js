2858937d3827c8d09a917829e8f37eb6abc45516
B11_Loai <loai.m.albadawi@gmail.com>
2024-07-25 19:01:42 +0300

    getRoles functions

---
+++ frontend/src/redux/reducers/Roles/Roles.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const roleSlice = createSlice({
+    name:"roles",
+    initialState:{
+        roles:[],
+        permissions:[],
+        rolePermissions:[],
+    },
+    reducers:{
+        getRoles : (state,action) =>{
+            state.roles = action.payload
+        },
+    
+    }
+})
