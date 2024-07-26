2611788c966065bca535e0723c645af78308002e
AhmadAzmii <ah.azmi.hasann@gmail.com>
2024-07-26 15:59:19 +0300

    fixing router.js so it render the owner Register and Owner login

--- frontend/src/Routers/Router.js
+++ frontend/src/Routers/Router.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import React from "react";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 
-import MainLayout from '../Layouts/MainLayout';
-import Carts from '../Pages/Carts/Carts';
-import Orders from '../Pages/Orders/Orders';
-import Reviews from '../Pages/Reviews/Reviews';
-import UserSettings from '../Pages/UserSettings/UserSettings';
-import AdminDashboard from '../Pages/AdminDashboard/AdminDashboard';
-import DriverDashboard from '../Pages/DriverDashboard/DriverDashboard';
-import Login from '../Pages/Login/Login';
-import OwnerLogin from '../Pages/OwnerLogin/OwnerLogin';
-import OwnerRegister from '../Pages/OwnerRegister/OwnerRegister';
-import Products from '../Pages/Products/Products';
-import Register from '../Pages/Register/Register';
-import ShopOwnerDashboard from '../Pages/ShopOwnerDashboard/ShopOwnerDashboard';
-import UserDashboard from '../Pages/UserDashboard/UserDashboard';
-import { useSelector } from 'react-redux';
-
+import MainLayout from "../Layouts/MainLayout";
+import Carts from "../Pages/Carts/Carts";
+import Orders from "../Pages/Orders/Orders";
+import Reviews from "../Pages/Reviews/Reviews";
+import UserSettings from "../Pages/UserSettings/UserSettings";
+import AdminDashboard from "../Pages/AdminDashboard/AdminDashboard";
+import DriverDashboard from "../Pages/DriverDashboard/DriverDashboard";
+import Login from "../Pages/Login/Login";
+import OwnerLogin from "../Pages/OwnerLogin/OwnerLogin";
+import OwnerRegister from "../Pages/OwnerRegister/OwnerRegister";
+import Products from "../Pages/Products/Products";
+import Register from "../Pages/Register/Register";
+import ShopOwnerDashboard from "../Pages/ShopOwnerDashboard/ShopOwnerDashboard";
+import UserDashboard from "../Pages/UserDashboard/UserDashboard";
+import { useSelector } from "react-redux";
 
 const ProtectedRoute = ({ element, role }) => {
   const { isLoggedIn, roleId } = useSelector((state) => state.auth);
@@ -35,45 +34,65 @@ const ProtectedRoute = ({ element, role }) => {
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Register />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
   {
-    path: '/',
-    element: <MainLayout />,
-    children :[
-{      path:'user-dashboard', element: <ProtectedRoute element={<UserDashboard />} role={1} />,
-}    ]
+    path: "owner-register",
+    element: <OwnerRegister />,
   },
   {
-    path: '/',
-    element: <MainLayout />,
-    children:[
-      { path: '/driver-dashboard',
-        element: <ProtectedRoute element={<DriverDashboard />} role={2} />,}
-    ]
+    path: "owner-login",
+    element: <OwnerLogin />,
   },
   {
-    path: '/',
+    path: "/",
     element: <MainLayout />,
     children: [
-      { path: 'carts', element: <ProtectedRoute element={<Carts />} /> },
-      { path: 'orders', element: <ProtectedRoute element={<Orders />} /> },
-      { path: 'reviews', element: <ProtectedRoute element={<Reviews />} /> },
-      { path: 'user-settings', element: <ProtectedRoute element={<UserSettings />} /> },
-      { path: 'admin-dashboard', element: <ProtectedRoute element={<AdminDashboard />} /> },
-      { path: 'driver-dashboard', element: <ProtectedRoute element={<DriverDashboard />} /> },
-      { path: 'login', element: <Login /> },
-      { path: 'owner-login', element: <OwnerLogin /> },
-      { path: 'owner-register', element: <OwnerRegister /> },
-      { path: 'products', element: <Products /> },
-      { path: 'register', element: <Register /> },
-      { path: 'shop-owner-dashboard', element: <ShopOwnerDashboard /> },
-      { path: 'user-dashboard', element: <UserDashboard /> },
+      {
+        path: "user-dashboard",
+        element: <ProtectedRoute element={<UserDashboard />} role={1} />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/driver-dashboard",
+        element: <ProtectedRoute element={<DriverDashboard />} role={2} />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "carts", element: <ProtectedRoute element={<Carts />} /> },
+      { path: "orders", element: <ProtectedRoute element={<Orders />} /> },
+      { path: "reviews", element: <ProtectedRoute element={<Reviews />} /> },
+      {
+        path: "user-settings",
+        element: <ProtectedRoute element={<UserSettings />} />,
+      },
+      {
+        path: "admin-dashboard",
+        element: <ProtectedRoute element={<AdminDashboard />} />,
+      },
+      {
+        path: "driver-dashboard",
+        element: <ProtectedRoute element={<DriverDashboard />} />,
+      },
+
+      { path: "products", element: <Products /> },
+
+      { path: "shop-owner-dashboard", element: <ShopOwnerDashboard /> },
+      { path: "user-dashboard", element: <UserDashboard /> },
     ],
   },
 ]);
