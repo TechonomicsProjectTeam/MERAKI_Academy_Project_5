d1cfa40006733fe6fd03136264e82a07c123c9f1
AhmadAzmii <ah.azmi.hasann@gmail.com>
2024-07-28 11:47:12 +0300

    adding get shopBy category slice

--- frontend/src/redux/reducers/Shops/Shops.js
+++ frontend/src/redux/reducers/Shops/Shops.js
@@ -1,41 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const shopsSlice = createSlice({
-    name:"shops",
-    initialState:{
-        shops:[],
+    name: "shops",
+    initialState: {
+        shops: [],
         images: localStorage.getItem("ShopImage") || null,
-        name: localStorage.getItem("name")|| null,
+        name: localStorage.getItem("name") || null,
     },
-    reducers:{
-        getShops: (state,action) =>{
-            state.shops = action.payload
+    reducers: {
+        getShops: (state, action) => {
+            state.shops = action.payload;
         },
-        addShops: (state, action)=>{
+        addShops: (state, action) => {
             state.shops.push(action.payload);
         },
-        deleteShopById: (state,action) =>{
-            //the id is sent from the payload
-            state.shops.splice(action.payload.shop_id,1);
+        deleteShopById: (state, action) => {
+            state.shops = state.shops.filter(shop => shop.shop_id !== action.payload.shop_id);
         },
-        updateShopById: (state,action)=>{
-            state.shops= state.shops.map((shop,index)=>{
-                if (shop.shop_id === action.payload.shop_id){
-                    shop = action.payload
-                }
-                return shop
-            })
+        updateShopById: (state, action) => {
+            state.shops = state.shops.map((shop) =>
+                shop.shop_id === action.payload.shop_id ? action.payload : shop
+            );
         },
-        setShopInfo: (state,action)=>{
-            const {name,images}=action.payload
+        setShopInfo: (state, action) => {
+            const { name, images } = action.payload;
             state.name = name;
             state.images = images;
-            localStorage.setItem("ShopImage",images)
-            localStorage.setItem("name",name)
-        }
-    }
-})
+            localStorage.setItem("ShopImage", images);
+            localStorage.setItem("name", name);
+        },
+        setShopsByCategory: (state, action) => {
+            state.shops = action.payload;
+        },
+    },
+});
 
-export const {getShops,updateShopById,addShops,deleteShopById,setShopInfo} = shopsSlice.actions;
+export const { getShops, updateShopById, addShops, deleteShopById, setShopInfo, setShopsByCategory } = shopsSlice.actions;
 
 export default shopsSlice.reducer;
