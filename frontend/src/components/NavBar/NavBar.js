a857eb48727af1ac95b708932ef6d8dc4303c1a4
AhmadAzmii <ah.azmi.hasann@gmail.com>
2024-08-01 13:20:15 +0300

    testing

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink ,useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { setLogout } from "../../redux/reducers/Auth/Auth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
@@ -8,7 +8,7 @@ import "../NavBar/Style.css";
 import { clearCartState } from "../../redux/reducers/Carts/Carts";
 const NavBar = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const token = useSelector((state) => state.auth.token);
   const username = useSelector((state) => state.auth.username);
   const imageUrl = useSelector((state) => state.auth.images);
@@ -18,101 +18,97 @@ const NavBar = () => {
   const handleLogout = () => {
     dispatch(setLogout());
     dispatch(clearCartState());
-    navigate("/user-dashboard")
+    navigate("/user-dashboard");
   };
   return (
-      <div className="NavBar">
-        <div className="user-info">
-          {imageUrl && <img src={imageUrl} alt="User" className="user-image" />}
-          {username && <span className="user-name">Welcome {username} !!</span>}
-        </div>
-        <div className="cart-icon">
-          {/* <NavLink to="/cart">
+    <div className="NavBar">
+      <div className="user-info">
+        {imageUrl && <img src={imageUrl} alt="User" className="user-image" />}
+        {username && <span className="user-name">Welcome {username} !!</span>}
+      </div>
+      <div className="cart-icon">
+        {/* <NavLink to="/cart">
             <FontAwesomeIcon icon={faShoppingCart} />
           </NavLink> */}
-        </div>
-        <nav>
-          {isLoggedIn ? (
-            parseInt(roleId) === 3 ? (
-              <>
-                <NavLink
-                  to="/shop-owner-dashboard"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  Shop Owner Dashboard
-                </NavLink>
-                <NavLink
-                  to="/products"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  AddProducts
-                </NavLink>
-                <NavLink to="/login" onClick={handleLogout}>
-                  Logout
-                </NavLink>
-              </>
-            ) : parseInt(roleId) === 2 ? (
-              <>
-                <NavLink
-                  to="/driver-dashboard"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  Driver Dashboard
-                </NavLink>
-                <NavLink to="/login" onClick={handleLogout}>
-                  Logout
-                </NavLink>
-              </>
-            ) : (
-              <>
-                <NavLink
-                  to="/user-dashboard"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  User Dashboard
-                </NavLink>
-                <NavLink
-                  to="/orders"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  Orders
-                </NavLink>
-                <NavLink
-                  to="/carts"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                   Cart
-                </NavLink>
-                <NavLink
-                  to="/reviews"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  Reviews
-                </NavLink>
-                <NavLink
-                  to="/user-settings"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                >
-                  User Settings
-                </NavLink>
-                <NavLink to="/login" onClick={handleLogout}>
-                  Logout
-                </NavLink>
-              </>
-            )
+      </div>
+      <nav>
+        {isLoggedIn ? (
+          parseInt(roleId) === 3 ? (
+            <>
+              <NavLink
+                to="/shop-owner-dashboard"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Shop Owner Dashboard
+              </NavLink>
+              <NavLink
+                to="/products"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                AddProducts
+              </NavLink>
+              <NavLink to="/login" onClick={handleLogout}>
+                Logout
+              </NavLink>
+            </>
+          ) : parseInt(roleId) === 2 ? (
+            <>
+              <NavLink
+                to="/driver-dashboard"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Driver Dashboard
+              </NavLink>
+              <NavLink to="/login" onClick={handleLogout}>
+                Logout
+              </NavLink>
+            </>
           ) : (
             <>
+              <NavLink
+                to="/user-dashboard"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                User Dashboard
+              </NavLink>
+              <NavLink
+                to="/orders"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Orders
+              </NavLink>
+              <NavLink
+                to="/carts"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Cart
+              </NavLink>
+              <NavLink
+                to="/reviews"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Reviews
+              </NavLink>
+              <NavLink
+                to="/user-settings"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                User Settings
+              </NavLink>
+              <NavLink to="/login" onClick={handleLogout}>
+                Logout
+              </NavLink>
+            </>
+          )
+        ) : (
+          <>
             {/* <NavLink to="/login">Login</NavLink> */}
             <NavLink to="/register">Register</NavLink>
-            </>
-          )}
-        </nav>
-      </div>
-    );
-  };
-  
-  export default NavBar;
-  
-  
-  
-  
+          </>
+        )}
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
