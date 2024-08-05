5d49e39787e5918b11dc0f121cdd241955174e3d
AhmadAzmii <ah.azmi.hasann@gmail.com>
2024-08-05 14:20:15 +0300

    fixing best rated shops rendering

--- backend/routes/Shop.js
+++ backend/routes/Shop.js
@@ -9,21 +9,22 @@ const {
   getShopById,
   getShopsByCategoryId,
   updateShopRating,
-  getBestRatedShops
+  getBestRatedShops,
+  getAllShopCities,
 } = require("../controllers/Shop");
 
 const shopRouter = express.Router();
 const authorization = require("../middlewares/authorization");
 const authentication = require("../middlewares/authentication");
 
-shopRouter.post("/",  createShops);
+shopRouter.post("/", createShops);
 shopRouter.get("/", authentication, getAllShops);
 shopRouter.put("/:id", updateShopById);
 shopRouter.delete("/:id", authentication, deleteShopsById);
 shopRouter.post("/shop_login", loginShop);
-shopRouter.get("/:id",getShopById);
-shopRouter.get("/category/:id",getShopsByCategoryId);
-shopRouter.post("/shops/rating",authentication, updateShopRating)
+shopRouter.get("/:id", getShopById);
+shopRouter.get("/category/:id", getShopsByCategoryId);
+shopRouter.post("/shops/rating", authentication, updateShopRating);
 shopRouter.get("/shops/best-rated", getBestRatedShops);
-
+shopRouter.get("/rated/shops", getAllShopCities);
 module.exports = shopRouter;
--- frontend/src/Pages/UserDashboard/BestRatedShops/BestRatedShops.js
+++ frontend/src/Pages/UserDashboard/BestRatedShops/BestRatedShops.js
@@ -13,12 +13,17 @@ const BestRatedShops = () => {
   const bestRatedShops = useSelector((state) => state.shops.bestRatedShops);
 
   const [message, setMessage] = useState("");
+  const [city, setCity] = useState("");
+  const [filteredShops, setFilteredShops] = useState([]);
 
   useEffect(() => {
     const fetchBestRatedShops = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/shop/shops/best-rated");
+        const response = await axios.get(
+          "http://localhost:5000/shop/shops/best-rated"
+        );
         if (response.data.success) {
+          console.log("Best Rated Shops fetched:", response.data.shops);
           dispatch(setBestRatedShops(response.data.shops));
         }
       } catch (error) {
@@ -28,6 +33,68 @@ const BestRatedShops = () => {
 
     fetchBestRatedShops();
   }, [dispatch]);
+
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error);
+      } else {
+        console.log("Geolocation is not supported by this browser.");
+        setMessage("Geolocation is not supported by this browser.");
+      }
+    };
+
+    const success = async (position) => {
+      const { latitude, longitude } = position.coords;
+      console.log("Geolocation success:", { latitude, longitude });
+
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/api/get-api-key`
+        );
+        const apiKey = response.data.apiKey;
+
+        const geoResponse = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`
+        );
+        console.log("Geocode API response:", geoResponse.data);
+
+        const addressComponents =
+          geoResponse.data.results[0]?.address_components;
+        const cityComponent = addressComponents?.find((component) =>
+          component.types.includes("locality")
+        );
+        if (cityComponent) {
+          setCity(cityComponent.long_name);
+          console.log("City found:", cityComponent.long_name);
+          filterShopsByCity(cityComponent.long_name);
+        } else {
+          setMessage("City name not found in geocoding results.");
+        }
+      } catch (error) {
+        console.error("Error fetching city name:", error);
+        setMessage("Error fetching city name: " + error.message);
+      }
+    };
+
+    const error = (err) => {
+      console.log("Unable to retrieve your location:", err);
+      setMessage("Unable to retrieve your location: " + err.message);
+    };
+    console.log(bestRatedShops);
+
+    const filterShopsByCity = (city) => {
+      console.log("Filtering shops by city:", city);
+      const filtered = bestRatedShops.filter((shop) => {
+        console.log(`Shop ${shop.name} city:`, shop.city);
+        return shop.city === city;
+      });
+      console.log("Filtered shops:", filtered);
+      setFilteredShops(filtered);
+    };
+
+    getLocation();
+  }, [bestRatedShops]);
 
   const handleShopClick = (shopId, shopName) => {
     axios
@@ -46,14 +113,17 @@ const BestRatedShops = () => {
   return (
     <div className="BestRatedShops">
       <h2>Best Rated Shops</h2>
+      {city && <p>Best rated shops in: {city}</p>}
       <ul className="best-rated-shop-list">
-        {bestRatedShops.map((shop) => (
+        {filteredShops.map((shop) => (
           <li
             key={shop.shop_id}
             onClick={() => handleShopClick(shop.shop_id, shop.name)}
           >
+            <img src={shop.image} alt={`${shop.name}`} />
             <h3>{shop.name}</h3>
             <p>Average Rating: {shop.average_rating}</p>
+            <p>{shop.description}</p>
           </li>
         ))}
       </ul>
