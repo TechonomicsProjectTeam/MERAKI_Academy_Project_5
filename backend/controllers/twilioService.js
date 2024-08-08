69333e791ab6d1a19141512cfcf970a7806ce926
AhmadAzmii <ah.azmi.hasann@gmail.com>
2024-08-08 20:04:15 +0300

    fixing something

--- backend/controllers/twilioService.js
+++ backend/controllers/twilioService.js
@@ -4,7 +4,7 @@ const client = twilio(process.env.TWILIO_AUTH, process.env.TWILIO_NUM);
 const sendSms = (phoneNumber, message) => {
     return client.messages.create({
         body: message,
-        from: '+15418358547', 
+        from: process.env.TWILIO_NUMBER, 
         to: phoneNumber,
     });
 };
